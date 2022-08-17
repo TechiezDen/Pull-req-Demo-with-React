6c6f793ae343a959245cf974e0bd88db9fe2c6c8
ngactive99 <ngactive99@gmail.com>
2022-08-17 23:23:29 +0530

    login text field and buttons added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
      <h1>Welcome to facebook</h1>
+     <input placeholder='Enter your username'></input>
+     <input placeholder='Enter your password'></input>
+     <button>Login</button>
+     <button>Sign up</button>
     </div>
   );
 }
